5b7adecca6ff784f04ef8f8e085e634fca5dbf26
Sudheendra Kumar Kaanugovi <sudhy.k@gmail.com>
2017-02-11 09:23:10 -0500

    Starter Project

---
+++ Nutrients/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nutrients
+//
+//  Created by Kaanugovi, Sudheendra Kumar on 2/11/17.
+//  Copyright © 2017 Kaanugovi, Sudheendra Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
